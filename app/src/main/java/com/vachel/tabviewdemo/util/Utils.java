67384db11ee0a3b8728b63a80a79166e364fccea
jianglixuan <jianglixuan@tik.com>
2019-08-20 11:08:12 +0800

    feat: first commit
    Signed-off-by: jianglixuan <jianglixuan@tik.com>

---
+++ app/src/main/java/com/vachel/tabviewdemo/util/Utils.java
@@ -0,0 +1,22 @@
+package com.vachel.tabviewdemo.util;
+
+public class Utils {
+
+    /**
+     * 颜色渐变计算
+     */
+    public static int evaluateColor(float fraction, int startValue, int endValue) {
+        int startA = (startValue >> 24) & 0xff;
+        int startR = (startValue >> 16) & 0xff;
+        int startG = (startValue >> 8) & 0xff;
+        int startB = startValue & 0xff;
+        int endA = (endValue >> 24) & 0xff;
+        int endR = (endValue >> 16) & 0xff;
+        int endG = (endValue >> 8) & 0xff;
+        int endB = endValue & 0xff;
+        return (startA + (int) (fraction * (endA - startA))) << 24
+                | (startR + (int) (fraction * (endR - startR))) << 16
+                | (startG + (int) (fraction * (endG - startG))) << 8
+                | (startB + (int) (fraction * (endB - startB)));
+    }
+}
